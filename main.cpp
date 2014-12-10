97b3161e8caaef312942f026b6d7cdf8ee828476
Dormouse <Dormouse@Dormouse-PC>
2014-12-10 16:17:24 +0000

    Added a useless comment

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Dormouse
+ *
+ * Created on 10 December 2014, 16:11
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+//oooh, actual code
